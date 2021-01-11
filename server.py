6147eb65958c83d409f7fb1fe441c09f857b6aa6
Jo-Anne Tan <ancientchronicles.anne@gmail.com>
2021-01-11 15:01:27 +0800

    added migrate function in server.py

--- server.py
+++ server.py
@@ -1,5 +1,8 @@
+from peeweedbevolve # new; must be imported before models
+
 from flask import Flask, render_template, request
 from models import db
+
 app = Flask(__name__)
 
 @app.before_request
@@ -10,6 +13,10 @@ def before_request():
 def after_request(response):
     db.close()
     return response
+
+@app.cli.command()
+def migrate():
+    db.evolve(ignore_tables = {'base_model'})
 
 @app.route("/")
 def index():
